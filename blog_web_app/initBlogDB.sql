d07498683034b984f9f12b747b8f15dcc93a9189
shirleyqi26 <69826368+shirleyqi26@users.noreply.github.com>
2023-11-23 22:51:09 -0800

    fixed bugs from practice demo today

--- blog_web_app/initBlogDB.sql
+++ blog_web_app/initBlogDB.sql
@@ -1,8 +1,9 @@
 DELETE FROM blogdb.posts WHERE postid;
-INSERT INTO blogdb.posts(subject, content, username) VALUES ('how does code work?', 'hello', 'smithbeast2');
-INSERT INTO blogdb.posts(subject, content, username) VALUES ('this is my post', 'stuff stuff stuff', 'secretman123');
-INSERT INTO blogdb.posts(subject, content, username) VALUES ('crypto', 'crypto', 'cryptolover5');
+INSERT INTO blogdb.posts(subject, content, username) VALUES ('Dont be like me...', 'Did you guys know that SMS isnt encrypted? I was texting my girlfriend my bank card information and password so that she can pay some bills, then a few hours later, somebody transferred $30,000 out of my chequing account to mall.icious@gmail.com. It was Mallory wasnt it... next time, Ill just tell my girlfriend in person. Remember the importants of encryption people!', 'Bob');
+INSERT INTO blogdb.posts(subject, content, username) VALUES ('I think Bob needs to take CPEN 442', 'Lately, Alice and Bob have been testing out MACs by sending messages back and forth. Today, Bob sent me a screenshot of a message that Alice allegedly sent him. How am I supposed to know if Alice really did send it? Both Bob and her have the key needed for MACs! I think Bob needs to go and study the concept of repudiation. I heard theres a great course that teaches repudiation at UBC called CPEN 442.', 'Carol');
+INSERT INTO blogdb.posts(subject, content, username) VALUES ('Step up your game, Alice!', 'I was bored today so I decided to perform a dictionary attack on Alices Facebook account. Unsurprisingly enough, I was able to crack it in just a couple of minutes. Next time Alice, think carefully before setting your password as "wonderland".', 'Mallory');
 DELETE FROM blogdb.users WHERE userid;
-INSERT INTO blogdb.users(username, name, birthday, email, secretinfo, password) VALUES ('christophersmith', 'Christopher Smith', '2000-01-01', 'christophersmith@gmail.com', 'Chris Smith', 'csmith');
-INSERT INTO blogdb.users(username, name, birthday, email, secretinfo, password) VALUES ('boatman', 'Boat Man', '2000-02-03', 'boatman@gmail.com', 'boatman', 'boatman1');
+INSERT INTO blogdb.users(username, name, birthday, email, secretinfo, password) VALUES ('Alice', 'Alice Alice', '2000-01-01', 'alice@gmail.com', 'I have over a million dollars in my bank account!', 'alice123');
+INSERT INTO blogdb.users(username, name, birthday, email, secretinfo, password) VALUES ('Mallory', 'Mallory Mallory', '2000-02-03', 'mall.icious@gmail.com', 'I will hack Alice today', 'mallory123');
+INSERT INTO blogdb.users(username, name, birthday, email, secretinfo, password) VALUES ('Bob', 'Bob Bob', '2000-03-23', 'bobby@gmail.com', 'Im tired of messaging Alice everyday...', 'bob123');
 DELETE FROM adversary_database.stolen_info WHERE id;
